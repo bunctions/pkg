cecf5204b66739f388b3806a487374f6b7fdf8f2
Ben Lei <ben181231@gmail.com>
2019-09-18 02:17:35 +0900

    Leave more comment for io package

--- function/io/input.go
+++ function/io/input.go
@@ -8,6 +8,8 @@ import (
 
 type ctxInputReaderKey struct{}
 
+// WithInputReader returns a copy of given context, with
+// the input readcloser injected.
 func WithInputReader(ctx context.Context, r io.ReadCloser) context.Context {
 	parent := ctx
 	if parent == nil {
@@ -17,6 +19,8 @@ func WithInputReader(ctx context.Context, r io.ReadCloser) context.Context {
 	return context.WithValue(parent, ctxInputReaderKey{}, r)
 }
 
+// InputReaderFromContext tries to extract the input reader from
+// the given context. If not available, an empty reader would be returned.
 func InputReaderFromContext(ctx context.Context) io.Reader {
 	emptyReader := strings.NewReader("")
 	if ctx == nil {
--- function/io/output.go
+++ function/io/output.go
@@ -8,6 +8,8 @@ import (
 
 type ctxOutputWriterKey struct{}
 
+// WithOutputWriter returns a copy of given context, with
+// the output writer injected.
 func WithOutputWriter(ctx context.Context, w io.Writer) context.Context {
 	parent := ctx
 	if parent == nil {
@@ -17,6 +19,8 @@ func WithOutputWriter(ctx context.Context, w io.Writer) context.Context {
 	return context.WithValue(parent, ctxOutputWriterKey{}, w)
 }
 
+// OutputWriterFromContext tries to extract the output writer from
+// the given context. If not available, an null writer would be returned.
 func OutputWriterFromContext(ctx context.Context) io.Writer {
 	nullWriter := ioutil.Discard
 	if ctx == nil {
